df10c6d645c576ee11541e30382982f1940d60f9
Ryan Bradshaw <bradshr@bc.edu>
2026-01-15 11:05:32 -0500

    Initial Commit

---
+++ FootballChallenge/FootballChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballChallengeApp.swift
+//  FootballChallenge
+//
+//  Created by Ryan Bradshaw on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
